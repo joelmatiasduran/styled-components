b7b02897f6e8de726b8aa8d8f8d25ecf1dd7f709
Joel Matias Duran <55359469+joelmatiasduran@users.noreply.github.com>
2021-05-13 16:56:17 -0300

    Small typo

--- src/contact.js
+++ src/contact.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
+            //Create one responsive
             <h1>Send me an email</h1>
             <p>Here there's supposed to be a form, I will make one</p>
             <p>Everything center and if it's a grid, I'm okay with it</p>
